804e38ce0323bdc11452d181b6c0d67428a49bf9
Mert <mertdemirok80@gmail.com>
2020-11-26 14:54:59 +0100

    refactor IceCreamAppV2 so that the customer can buy ice cream that are in stock

--- src/be/intecbrussel/application/IceCreamAppV2.java
+++ src/be/intecbrussel/application/IceCreamAppV2.java
@@ -8,31 +8,56 @@ import be.intecbrussel.sellers.*;
 public class IceCreamAppV2 {
     public static void main(String[] args) {
         PriceList priceList = new PriceList(2, 1.5, 2.5);
-        Stock stock = new Stock(2, 2, 8, 2);
+        Stock stock = new Stock(1, 1, 8, 1);
         IceCreamSeller iceCreamSeller = new IceCreamCar(priceList, stock);
 
         Cone.Flavor[] balls = {Cone.Flavor.BANANA, Cone.Flavor.CHOCOLATE, Cone.Flavor.MOKKA};
 
-        try{
+        Eatable[] orders = new Eatable[10];
+        try {
             Eatable order1 = iceCreamSeller.orderCone(balls);
-            Eatable order2 = iceCreamSeller.orderCone(balls);
-            Eatable order3 = iceCreamSeller.orderCone(balls); // out of stock”
-            Eatable order4 = iceCreamSeller.orderIceRocket();
-            Eatable order5 = iceCreamSeller.orderIceRocket();
-            Eatable order6 = iceCreamSeller.orderIceRocket(); // out of stock
-            Eatable order7 = iceCreamSeller.orderMagnum(Magnum.MagnumType.ALPINE_NUTS);
-            Eatable order8 = iceCreamSeller.orderMagnum(Magnum.MagnumType.ROMANTICS_STRAWBERRIES);
-            Eatable order9 = iceCreamSeller.orderMagnum(Magnum.MagnumType.BLACK_CHOCOLATE); // out of stock
-            Eatable[] orders = {order1, order2, order3, order4, order5, order6, order7, order8, order9};
-            for (Eatable iceCream : orders) {
-                if(iceCream != null){
-                    iceCream.eat();
-                }
-            }
-            System.out.println("Total Profit: " + iceCreamSeller.getProfit());
+            orders[0] = order1;
         } catch (Exception e){
-            System.out.println(e.getMessage());
             e.printStackTrace();
         }
+        try {
+            Eatable order2 = iceCreamSeller.orderCone(balls);
+            orders[1] = order2;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+            Eatable order3 = iceCreamSeller.orderIceRocket();
+            orders[2] = order3;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+            Eatable order4 = iceCreamSeller.orderIceRocket();
+            orders[3] = order4;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+            Eatable order5 = iceCreamSeller.orderMagnum(Magnum.MagnumType.ALPINE_NUTS);;
+            orders[4] = order5;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+            Eatable order6 = iceCreamSeller.orderMagnum(Magnum.MagnumType.BLACK_CHOCOLATE);
+            orders[4] = order6;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        for (Eatable iceCream : orders) {
+            if(iceCream instanceof Exception){
+                continue;
+            }
+            if(iceCream != null){
+                iceCream.eat();
+            }
+        }
+        System.out.println("Total Profit: " + iceCreamSeller.getProfit());
     }
 }
